2733fb796e5deabcc194b3e7de8743086d0ef940
halvorboe <halvor@highered.global>
2020-05-09 18:27:18 +0200

    Fixed writing flow

--- sync.py
+++ sync.py
@@ -4,12 +4,16 @@ import os
 import subprocess
 import json
 from datetime import datetime
+import re
+from pprint import pprint
+import requests
 
 import tabulate
 
 IN_PATH = "/home/dev/writings"
 OUT_PATH = "/home/dev/projects/writings/posts"
 CONTENT_PATH = "/home/dev/projects/complex.codes/html/content/posts"
+GIST_REGEX = r'https:\/\/gist.githubusercontent.com\/[A-Za-z0-9]*\/[A-Za-z0-9]*\/raw\/[A-Za-z0-9]*\/[A-Za-z0-9]*\...'
 
 def extract_path(raw_path):
     path = clean_raw_path(raw_path)
@@ -32,10 +36,11 @@ def open_config(local_path, title=None):
     except FileNotFoundError:
         print("No config found...")
     finally:
-        final_config = build_config(config, title=title)
+        if not config:
+            config = build_config(config, title=title)
         with open(config_path, "w+") as f:
-            f.write(json.dumps(final_config)) 
-        return final_config
+            f.write(json.dumps(config)) 
+        return config
 
 def build_config(config, title=None):
     return {
@@ -65,16 +70,30 @@ def main():
         subprocess.run(["pandoc", "-s", raw_path, "-t", "markdown", "-o", f"{local_path}/raw.md"])
         config = open_config(local_path, title=title)
         writings.append([title, path, False])
-        content_path = f"{CONTENT_PATH}/{path}"
-        mkdir(content_path)
-        with open(f"{content_path}/index.md", "w+") as f:
+
+        with open(f"{local_path}/index.md", "w+") as f:
             f.write("---\n")
             for key, value in config.items():
                 f.write(f"{key}: {json.dumps(value)}\n")
             f.write("---\n")
             with open(f"{local_path}/raw.md") as inp:
                 for line in inp:
-                    f.write(line)
+                    if re.search(GIST_REGEX, line): 
+                        match = re.search(GIST_REGEX, line).group(0)                        
+                        code = requests.get(match).text
+                        language = match.split(".")[-1]
+                        f.write(f"```{language}\n")
+                        for line in code:
+                            f.write(line)
+                        f.write(f"```\n")
+                    else:
+                        f.write(line)
+        content_path = f"{CONTENT_PATH}/{path}"
+        mkdir(CONTENT_PATH)
+        mkdir(content_path)
+        with open(f"{local_path}/index.md", "r") as inp:
+            with open(f"{content_path}/index.md", "w+") as out:
+                out.write(inp.read())
         
 
     print(tabulate.tabulate(writings, headers="firstrow"))
